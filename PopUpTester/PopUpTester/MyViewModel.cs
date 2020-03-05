0cbd2ea509bfb140e15dc3875931bdc5432ed28a
Ricardo Cardona <ricardo.augusto.cardona@gmail.com>
2020-03-05 00:07:09 -0300

    Adicionar arquivos de projeto.

---
+++ PopUpTester/PopUpTester/MyViewModel.cs
@@ -0,0 +1,39 @@
+﻿
+using MvvmHelpers;
+using Rg.Plugins.Popup.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace PopUpTester
+{
+    public class MyViewModel : BaseViewModel
+    {
+        public ICommand AcaoCommand => new Command(async () => { await Acao(); });
+        public ICommand AcaoPopUpCommand => new Command(async () => { await AcaoPopUp(); });
+
+        private string _texto = "here we go!";
+        public string Texto
+        {
+            get => _texto;
+            set => SetProperty(ref _texto, value);
+        }
+
+        public async Task Acao()
+        {
+            await PopupNavigation.Instance.PushAsync(new PopUp.PopUpView(this));
+            await App.Current.MainPage.DisplayAlert("", "Deu bom!!", "Ok");
+            Texto = "All fine";
+        }
+
+        public async Task AcaoPopUp()
+        {
+            Texto = "PopUp Actions!!!!!! xD";
+            await PopupNavigation.Instance.PopAsync();
+        }
+
+    }    
+}
---
+++ PopUpTester/PopUpTester/PopUp/PopUpView.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using MvvmHelpers;
+using Rg.Plugins.Popup.Pages;
+using Rg.Plugins.Popup.Services;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PopUpTester.PopUp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PopUpView : PopupPage
+    {
+        public PopUpView(MyViewModel myViewModel)
+        {
+            InitializeComponent();
+            BindingContext = myViewModel;
+        }
+    }
+}
